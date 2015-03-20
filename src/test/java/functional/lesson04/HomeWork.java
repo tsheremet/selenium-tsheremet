8500009250a2d11093fab1b141e3ffb21f3363db
tsheremet <tanya@Tetianas-MacBook-Pro.local>
2015-03-20 18:42:31 +0200

    HomeWork-01

--- src/test/java/functional/lesson04/HomeWork.java
+++ src/test/java/functional/lesson04/HomeWork.java
@@ -1,6 +1,6 @@
 package functional.lesson04;
 
-import org.apache.commons.logging.Log;
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,15 +12,14 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.logging.Logger;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.BasicConfigurator;
 
 /**
  * Created by tanya on 3/17/15.
  *
  * 1. Написать тест который Не находит элемент на страничке и падает с exception, и
- * написать второй который Не находит элемент на страницу и НЕ падает с Exception.
+ * написать второй который Не находит элемент на странице и НЕ падает с Exception.
  * Драйвер должен корректно закрываться не зависимо от результатов теста.
  * (findElements, BeforeSuite, AfterSuite в testNG)
  * домашки сдавать как проекты на gitHub
@@ -29,18 +28,22 @@ import org.apache.log4j.BasicConfigurator;
 
 public class HomeWork {
     private WebDriver driver;
-    //private static Logger Log = Logger.getLogger(HomeWork.class);
+    private static Logger Log = Logger.getLogger(HomeWork.class.getName());
+    private static final By notExistedElement = By.xpath("//div[@id='gjsghdskj']");
     private static final By newsSectionHeader = By.xpath("//div[@id='news_list']/section[@class='items']/h2");
-    private static final String newsSectionHeaderLink = "//a[contains(text(),'%s')]";
+    public final String URL = "http://ukr.net";
 
     @DataProvider
-    public Object[][] keywords() {
+    public Object[][] keywordExist() {
         return new Object[][]{
-                new Object[]{"Технdsології",""},
-                new Object[]{"За коdsрдоном", "exception"},
-                /*new Object[]{"Технології",""},
-                new Object[]{"За кордоном", "exception"},
-                new Object[]{"Курйози", "exception 1"}*/
+                new Object[]{"Технології"}
+        };
+    }
+
+    @DataProvider
+    public Object[][] keywordNotExist() {
+        return new Object[][]{
+                new Object[]{"Техноdsлогії"}
         };
     }
 
@@ -49,29 +52,54 @@ public class HomeWork {
         driver = new FirefoxDriver();
     }
 
-    @Test(dataProvider = "keywords")
-    public void verifyElementsPresent(String keyword, String except) {
-        driver.get("http://ukr.net");
-        BasicConfigurator.configure();
-        //Log.info("I'm on page");
+    @Test(expectedExceptions = NoSuchElementException.class)
+    public void findElementWithException1() {
 
-        List<WebElement> newsSectionHeadersList = driver.findElements(newsSectionHeader);
-        for (WebElement newsSectionHeaderElement : newsSectionHeadersList) {
-            System.out.println(newsSectionHeaderElement.getText());
+        boolean elementPresent=false;
 
-            //Assert.assertTrue(newsSectionHeaderElement.findElement(By.xpath(String.format(newsSectionHeaderLink,keyword))).isDisplayed(),String.format("Element '%s' not found.",keyword));
-            Assert.assertFalse(newsSectionHeaderElement.findElement(By.xpath(String.format(newsSectionHeaderLink, keyword))).isDisplayed(), String.format("Element '%s' found.", keyword));
+        driver.get("http://www.amazon.com/");
+        WebElement element = driver.findElement(By.cssSelector(".nav-input[class='nav']"));
+        elementPresent = true;
+        Assert.assertTrue(!elementPresent);
+
+    }
+
+    @Test
+    public void checkElementPresentWithException() {
+        Log.info("Test 'checkElementPresentWithException' starts");
+        driver.get(URL);
+        driver.findElement(notExistedElement).isDisplayed();
+        Log.info("Element Not Found");
+    }
+
+    @Test(dataProvider = "keywordNotExist")
+    public void verifyElementPresent(String keyword) {
+        Log.info("Test 'verifyElementPresent' starts");
+        driver.get(URL);
+        try {
+            List<WebElement> newsList = driver.findElements(newsSectionHeader);
+            for (WebElement newsElement : newsList) {
+                //Log.info(newsElement.getText());
+                Assert.assertNotEquals(newsElement.getText(), keyword, String.format("Element '%s' was found", keyword));
+            }
+            Log.info("Element Not Found");
+        } catch (ElementNotFoundException elementNotFoundException) {
+            Log.info("ElementNotFoundException thrown: " + elementNotFoundException);
+        } catch (NoSuchElementException noSuchElementException) {
+            System.out.println("NoSuchElementException thrown: " + noSuchElementException);
         }
     }
 
-    public boolean isElementExists(By by) {
-        boolean isExists = true;
-        try {
-            driver.findElement(by);
-        } catch (NoSuchElementException e) {
-            isExists = false;
+    @Test(dataProvider = "keywordNotExist")
+    public void verifyElementPresentWithException(String keyword) {
+        Log.info("Test 'verifyElementPresentWithException' starts");
+        driver.get(URL);
+        List<WebElement> newsList = driver.findElements(newsSectionHeader);
+        for (WebElement newsElement : newsList) {
+            //Log.info(newsElement.getText());
+            Assert.assertEquals(newsElement.getText(),keyword,String.format("Element '%s' was found",keyword));
         }
-        return isExists;
+        Log.info("Element Not Found");
     }
 
     @AfterTest
